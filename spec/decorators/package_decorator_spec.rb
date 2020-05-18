9cb5bf8e3a5ebb854f5bc7f2f26e11cedfee09f9
Daniel J. Pritchett <dpritchett@gmail.com>
2020-05-18 13:29:09 -0500

    Adds spec for package address verification errors

--- spec/decorators/package_decorator_spec.rb
+++ spec/decorators/package_decorator_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
+require 'ostruct'
 
 describe Spree::Stock::Package, :vcr do
   let(:package) { create(:shipment).to_package }
@@ -31,5 +32,44 @@ describe Spree::Stock::Package, :vcr do
       expect(EasyPost::Shipment).to receive(:create).with(anything)
       subject
     end
+  end
+
+  describe '#easypost_address' do
+    let(:package) { create(:shipment, :invalid_address).to_package }
+    let(:order) { double 'order' }
+    let(:ship_address ) { double 'ship_address' }
+    let(:easy_address) { double 'easy address' }
+
+    let(:invalid_address_error) { OpenStruct.new message: "it's a bad address!" }
+    let(:delivery_verification) { OpenStruct.new success: false, errors: [invalid_address_error] }
+    let(:verifications) { OpenStruct.new delivery: delivery_verification }
+
+    let(:model_errors) { ActiveModel::Errors.new(order) }
+
+    before do
+      allow(package).to receive(:order).and_return(order)
+      allow(order).to receive(:ship_address).and_return(ship_address)
+      allow(ship_address).to receive(:easypost_address).and_return(easy_address)
+      allow(easy_address).to receive(:verifications).and_return(verifications)
+      allow(order).to receive(:errors).and_return(model_errors)
+      ::Spree::Easypost::Config.address_verification_enabled = true
+    end
+
+    context 'unverifiable address' do
+      it 'mentions easypost in its order errors object' do
+        package.easypost_address
+
+        expect(order.errors).not_to be_empty
+
+        errors = order.errors
+        expect(errors).to be_a ActiveModel::Errors
+
+        err_msg = errors[:address].join
+
+        expect(err_msg).to include package.error_label
+        expect(err_msg).to include invalid_address_error.message
+      end
+    end
+
   end
 end
--- spec/factories/spree_modification.rb
+++ spec/factories/spree_modification.rb
@@ -21,5 +21,21 @@ FactoryBot.modify do
     city { 'Manville' }
     association(:state, name: 'New Jersey', abbr: 'NJ')
     zipcode { '08835' }
+
+  end
+
+  factory :stock_location do
+    trait :invalid do
+      address1 { '10000 Flamingo Rd.'}
+      city { 'Gulf Breeze' }
+      association(:state, name: 'Florida', abbr: 'FL')
+      zipcode { rand(10000..99999).to_s }
+    end
+  end
+
+  factory :shipment do
+    trait :invalid_address do
+      association(:stock_location, :invalid)
+    end
   end
 end
